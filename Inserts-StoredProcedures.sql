db231b8c5107964fe751f4be44aba69123fb0773
Fernando <nando9606@gmail.com>
2020-12-17 20:08:21 -0600

    Se inserta el query

---
+++ Inserts-StoredProcedures.sql
@@ -0,0 +1,22 @@
+﻿-----------------INSERT CARRERAS-----------------
+
+ INSERT INTO CARRERAS(NOMBRE)
+ VALUES('Ingeniería de Software')
+
+ INSERT INTO CARRERAS(NOMBRE)
+ VALUES('Administración de Empresas')
+
+ INSERT INTO CARRERAS(NOMBRE)
+ VALUES('Ingeniería en Sistemas')
+
+ INSERT INTO CARRERAS(NOMBRE)
+ VALUES('Derecho')
+
+ INSERT INTO CARRERAS(NOMBRE)
+ VALUES('Diseñador Grafico')
+
+ INSERT INTO CARRERAS(NOMBRE)
+ VALUES('Ingeniería Electrónica')
+
+
+ SELECT * FROM CARRERAS
